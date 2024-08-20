bbd44f7649076f37f9cc004174ce5834087eee36
HTigee <147353617+HTigee@users.noreply.github.com>
2024-08-20 19:07:51 -0400

    separate graph for immune response

--- HALModeling2024Graphs.py
+++ HALModeling2024Graphs.py
@@ -14,16 +14,14 @@ class HALModeling2024Graphs:
     triggering_cells = df['TriggeringCells']
     tumor_cells = df['TumorCells']
     doomed_cells = df['DoomedCells']
-    immune_response = df['ImmuneResponse']
 
+    # First graph
     plt.figure(figsize=(8,6))
     plt.plot(Timestep, lymphocyte_cells, label='Lymphocytes', marker='o', color='blue', markersize = 1)
     plt.plot(Timestep, triggering_cells, label='Triggering Cells', marker='*', color='green', markersize=1)
     plt.plot(Timestep, tumor_cells, label='Tumor Cells', marker='s', color='red', markersize = 1)
     plt.plot(Timestep, doomed_cells, label='Doomed Cells', marker='^', color='gold', markersize = 1)
-    plt.plot(Timestep, immune_response, label='Immune Response', marker='p', color='black', markersize=1)
 
-    #plt.title('Cell Counts Over Time')
     plt.xlabel('Timestep')
     plt.ylabel('Cell Count')
 
@@ -32,4 +30,20 @@ class HALModeling2024Graphs:
     save_path = r'C:\Users\Hannah\Documents\HALModeling2024Outs\TrialRunGraph.png'
     plt.savefig(save_path, dpi=300, bbox_inches='tight')
 
+    #plt.show()
+
+    # Second graph
+    primary_immune_response = df['PrimaryImmuneResponse']
+    secondary_immune_response = df['SecondaryImmuneResponse']
+    immune_response = df['ImmuneResponse']
+    plt.figure(figsize=(8, 6))
+    plt.plot(Timestep, immune_response, label='Total Immune Response', marker='o', color='black', markersize=1)
+    plt.plot(Timestep, primary_immune_response, label='Primary Immune Response', marker='s', color='purple', markersize=1)
+    plt.plot(Timestep, secondary_immune_response, label='Secondary Immune Response', marker='^', color='brown', markersize=1)
+    plt.xlabel('Timestep')
+    plt.ylabel('Immune Response')
+    plt.legend()
+    plt.grid(True)
+    save_path = r'C:\Users\Hannah\Documents\HALModeling2024Outs\TrialRunGraphImmuneResponse.png'
+    plt.savefig(save_path, dpi=300, bbox_inches='tight')
     plt.show()
